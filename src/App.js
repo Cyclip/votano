a3a7faca996729ceafcfea74a8975ce78779e154
Cyclip <gdneop@gmail.com>
2022-01-12 21:56:38 +0000

    Create menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import './fonts.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// All icon imports
+import {
+    MenuAlt3Icon,
+    ChevronDownIcon,
+} from '@heroicons/react/outline'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            menuOpen: true,
+            playlistsOpen: true,
+        };
+    }
+
+    toggleMenu = () => {
+        this.setState({
+            menuOpen: !this.state.menuOpen,
+        });
+    }
+
+    togglePlaylists = () => {
+        this.setState({
+            playlistsOpen: !this.state.playlistsOpen,
+        });
+    }
+
+    render() {
+        return <div className="App"> 
+            <header className="App-header">
+                <div className="navbar">
+                    <div className="menuButton center-content" onClick={() => this.toggleMenu()}>
+                        <MenuAlt3Icon className="icon"/>
+                    </div>
+                    <h1 className='title no-select'>Votano</h1>
+                </div>
+                <div className="contents">
+                    <div className={"menu " + (this.state.menuOpen ? "open" : "closed")}>
+                        <div className="items no-select">
+                        <div className="item center-left-content">
+                            <h3>Search</h3>
+                        </div>
+                        <div className="item" onClick={() => this.togglePlaylists()}>
+                            <div class="name center-left-content withIcon">
+                                <h3>My playlists</h3>
+                            </div>
+                            <ChevronDownIcon class={"icon " + (this.state.playlistsOpen ? "flipped" : "unflipped")}/>
+                        </div>
+                        
+                        </div>
+                    </div>
+                    <div class="main">
+                        
+                    </div>
+                </div>
+            </header>
+        </div>;
+    };
 }
 
 export default App;
